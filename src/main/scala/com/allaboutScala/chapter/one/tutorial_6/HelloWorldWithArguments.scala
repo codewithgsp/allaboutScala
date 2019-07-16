6ce61edfdae629f91a9ae20123a68b50ce4c4f9b
Saiprasadgorre <dare2dream30@gmail.com>
2019-07-16 22:16:09 +0200

    Created HelloWorldMain.scala

---
+++ src/main/scala/com/allaboutScala/chapter/one/tutorial_6/HelloWorldWithArguments.scala
@@ -0,0 +1,15 @@
+package com.allaboutScala.chapter.one.tutorial_6
+
+/**
+  * HelloWorldWithArguments
+  */
+object HelloWorldWithArguments extends App{
+
+  println("Hello World with Arguments Scala Application")
+  println("Command line arguments are :")
+
+  // args is a collection of arguments {Array[String] => collections}
+  // to make a collection to String, use s.mkString(sep=",")
+
+  println(args.mkString(sep = ", "))
+}
