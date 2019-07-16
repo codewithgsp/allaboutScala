8fa8f800189e5ff2669fdb24f7d465f8ae9d76d2
Saiprasadgorre <dare2dream30@gmail.com>
2019-07-16 21:53:40 +0200

    Created HelloWorldMain.scala

---
+++ src/main/scala/com/allaboutScala/chapter/one/tutorial_5/HelloWorldMain.scala
@@ -0,0 +1,12 @@
+package com.allaboutScala.chapter.one.tutorial_5
+
+/**
+  * Running scala program using main()
+  */
+object HelloWorldMain {
+
+  def main(args: Array[String]): Unit = {
+    print("Hello World from the main function!")
+  }
+
+}
