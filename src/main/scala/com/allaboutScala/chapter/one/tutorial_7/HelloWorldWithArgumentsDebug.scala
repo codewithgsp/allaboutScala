f535e4fd43e32e0e8ac83627abfdace20c34b620
Saiprasadgorre <dare2dream30@gmail.com>
2019-07-17 20:27:15 +0200

    Created HelloWorldWithArgumentsDebug.scala

---
+++ src/main/scala/com/allaboutScala/chapter/one/tutorial_7/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,12 @@
+package com.allaboutScala.chapter.one.tutorial_7
+
+/**
+  * print Arguments with debugger
+  */
+object HelloWorldWithArgumentsDebug extends App{
+
+  println("Hello World with Arguments Scala Application")
+  println("Command line arguments are: ")
+  println(args.mkString(sep = ", "))
+
+}
